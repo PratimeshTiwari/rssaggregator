47d1132f9dbc9e914873355fc5e898f6c2dcb222
Pratimesh Tiwari <pratimesh2004@gmail.com>
2025-05-28 16:52:34 +0530

    Added Router and Handlers

--- main.go
+++ main.go
@@ -31,6 +31,11 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Get("/error", handlerErr)
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
